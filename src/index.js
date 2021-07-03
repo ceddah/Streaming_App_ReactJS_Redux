380801eacd18c4622d7b47a0fc41c1a8952143cd
ceddah <cedanik1@gmail.com>
2021-07-03 14:08:41 +0200

    Google Auth Working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import reducers from './reducers'
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
 
 import App from './components/App';
 
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers
+    // composeEnhancers
+);
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
